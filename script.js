fd7027b0b4e147b0ba87005954c55727e4744ee4
badrbelabrik <badr.belabrik@gmail.com>
2026-01-09 17:56:40 +0100

    generate ticket function

--- script.js
+++ script.js
@@ -10,67 +10,74 @@ function getSpots(){
 function setSpots(spots){
     localStorage.setItem("spots",JSON.stringify(spots))
 }
-const spots = [{
-number: "A01",
-occupied: false
-},
-{
-number: "A02",
-occupied: false
-},
-{
-number: "A03",
-occupied: false
-},
-{
-number: "A04",
-occupied: false
-},
-{
-number: "A05",
-occupied: false
-},
-{
-number: "B01",
-occupied: false
-},
-{
-number: "B02",
-occupied: false
-},
-{
-number: "B03",
-occupied: false
-},
-{
-number: "B04",
-occupied: false
-},
-{
-number: "B05",
-occupied: false
-},
-{
-number: "C01",
-occupied: false
-},
-{
-number: "C02",
-occupied: false
-},
-{
-number: "C03",
-occupied: false
-},
-{
-number: "C04",
-occupied: false
-},
-{
-number: "C05",
-occupied: false
-},
-]
+function getTickets(){
+    return JSON.parse(localStorage.getItem("tickets")) || []
+}
+function setTickets(tickets){
+    localStorage.setItem("tickets",JSON.stringify(tickets))
+}
+// const spots = [{
+// number: "A01",
+// occupied: false
+// },
+// {
+// number: "A02",
+// occupied: false
+// },
+// {
+// number: "A03",
+// occupied: false
+// },
+// {
+// number: "A04",
+// occupied: false
+// },
+// {
+// number: "A05",
+// occupied: false
+// },
+// {
+// number: "B01",
+// occupied: false
+// },
+// {
+// number: "B02",
+// occupied: false
+// },
+// {
+// number: "B03",
+// occupied: false
+// },
+// {
+// number: "B04",
+// occupied: false
+// },
+// {
+// number: "B05",
+// occupied: false
+// },
+// {
+// number: "C01",
+// occupied: false
+// },
+// {
+// number: "C02",
+// occupied: false
+// },
+// {
+// number: "C03",
+// occupied: false
+// },
+// {
+// number: "C04",
+// occupied: false
+// },
+// {
+// number: "C05",
+// occupied: false
+// },
+// ]
+
 
 function createSpots(){
   const spots = getSpots() || []
@@ -88,7 +95,7 @@ function createSpots(){
             spotDiv.id = `${spots[i].number}`
             spotDiv.className=("flex-none w-[calc(20%-12px)] h-20 bg-[#3468B5] border rounded-3xl flex items-center justify-center cursor-pointer")
             spotDiv.onclick = () => {
-              showModal(i); 
+              showModal(spots[i]); 
             };
             spotDiv.innerHTML = `
             <span class="text-white text-xs font-bold">${spots[i].number}</span>
@@ -116,51 +123,106 @@ function bookSpot(){
   const availableSpot = spots.find(item => item.occupied == false)
   if (availableSpot){
     const date = new Date()
-    const creationDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}-${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`
+    const creationDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
+    const entryHour = `${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`
     const platenumber = document.getElementById("plateNumber")
     const vehiculetype = document.getElementById("vehiculeType")
-    const newVehicule = {plateNumber:platenumber.value,type:vehiculetype.value,slotNumber:availableSpot.number,entryTime:creationDate}
+    const newVehicule = {plateNumber:platenumber.value,type:vehiculetype.value,slotNumber:availableSpot.number,creationDate:creationDate,entryTime:entryHour,exitTime: null}
     const spot = document.getElementById(availableSpot.number)
     availableSpot.occupied = true
     vehicules.push(newVehicule)
     setVehicules(vehicules)
     setSpots(spots)
+
+    createSpots();
+
   } else {
     console.log("there is no available spot")
   }
 }
 
 
-function releaseSpot(index){
-  const spots = getSpots()
-  spots[index].occupied = false
-  setSpots(spots)
+function getTicket(spot,vehicule){
+    const vehicules = getVehicules()
+    const spots = getSpots()
+    const tickets = getTickets()
+    const date = new Date()
+    const exitHour = `${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`
+    
+    const vehiculeFound = vehicules.find(item => item.plateNumber == vehicule.plateNumber)
+    const spotFound = spots.find(item => item.number == spot.number)
+    vehiculeFound.exitTime = exitHour
+  
+    spotFound.occupied = false
+    setSpots(spots)
+     
+    const price = priceCalculation(vehicule.entryTime,exitHour)
+    const ticket = {slot:spot.number,plateNumber:vehiculeFound.plateNumber,type:vehicule.type,entryTime:vehicule.entryTime,exitTime:vehiculeFound.exitTime,price:price}
+    tickets.push(ticket)
+    setTickets(tickets)
+    closeModal()
+    createSpots()
+    const index = vehicules.indexOf(vehiculeFound)
+    if (index !== -1) {
+    vehicules.splice(index, 1);
+        }
+    setVehicules(vehicules)
+  }
+
+function priceCalculation(entry,exit){
+    const entryParts = entry.split(":")
+    const exitParts = exit.split(":")
+    const entryHour = parseInt(entryParts[0], 10)
+    const entryMin  = parseInt(entryParts[1], 10)
+    const exitHour = parseInt(exitParts[0],10)
+    const exitMin = parseInt(exitParts[1],10)
+
+    const entryDuration = (entryHour*60)+entryMin
+    const exitDuration = (exitHour*60)+exitMin
+    const totalDuration = (Math.floor((exitDuration-entryDuration)/60))
+    let price = 0;
+    if(totalDuration<=1){
+      price = 5;
+    } else {
+      price = 5+((totalDuration-1)*3)
+    }
+    return price
 }
 
-let currentSelectedSpot = null; // To keep track of which spot is open
 
-function showModal(index) {
+function showModal(currentSpot) {
     const modal = document.getElementById("infoModal");
     const spots = getSpots()
     const vehicules = getVehicules()
-    const currentSpot = spots[index]
+    // const currentSpot = spots[index]
     const currentVehicule = vehicules.find(item=>item.slotNumber == currentSpot.number)
     if(currentSpot.occupied){
-          document.getElementById("modalSpotId").innerText = currentSpot.number;
+    const date = new Date()
+    const exitHour = `${date.getHours()}:${date.getMinutes().toString().padStart(2, "0")}`
+    document.getElementById("modalSpotId").innerText = currentSpot.number;
+    document.getElementById("modalDate").innerText = currentVehicule.creationDate || "Empty";      
     document.getElementById("modalPlate").innerText = currentVehicule.plateNumber || "Empty";
     document.getElementById("modalType").innerText = currentVehicule.type || "N/A";
-    document.getElementById("modalTime").innerText = currentVehicule.entryTime || "Not checked-in";
+    document.getElementById("modalEntryTime").innerText = currentVehicule.entryTime || "Not checked-in";
+    document.getElementById("modalExitTime").innerText = exitHour || "Not checked-in"; 
+    document.getElementById("modalPrice").textContent = `${priceCalculation(currentVehicule.entryTime,exitHour)} MAD` || "Not checked-in"; 
     }else{
-                document.getElementById("modalSpotId").innerText = currentSpot.number;
+    document.getElementById("modalSpotId").innerText = currentSpot.number;
+    document.getElementById("modalDate").innerText = "Empty";
     document.getElementById("modalPlate").innerText = "Empty";
     document.getElementById("modalType").innerText = "N/A";
-    document.getElementById("modalTime").innerText = "Not checked-in";
+    document.getElementById("modalEntryTime").innerText = "Not checked-in";
+    document.getElementById("modalExitTime").innerText = "Not checked-in";
+    document.getElementById("modalPrice").innerText = "Not checked-in";
     }
     // 2. Fill the text fields with the data from the 'spots' array
 
 
     // 3. Show/Hide the Release button based on status
-    const releaseBtn = modal.querySelector('button[onclick="releaseSpot()"]');
+    const releaseBtn = document.getElementById("getTicket");
+    releaseBtn.onclick = () => {
+      getTicket(currentSpot,currentVehicule)
+    }
     releaseBtn.style.display = currentSpot.occupied ? "block" : "none";
 
     // 4. Open the native dialog
@@ -171,9 +233,12 @@ function closeModal() {
     document.getElementById("modalSpotId").innerText = "";
     document.getElementById("modalPlate").innerText = "";
     document.getElementById("modalType").innerText = "";
-    document.getElementById("modalTime").innerText = "";
+    document.getElementById("modalEntryTime").innerText = "";
+    document.getElementById("modalExitTime").innerText = "";
+    document.getElementById("modalPrice").innerText = "";
     document.getElementById("infoModal").close();
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     createSpots()
